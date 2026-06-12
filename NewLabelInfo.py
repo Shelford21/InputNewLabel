c825a65e853929fd5cd025cc2f8dedbc909b476d
Fauzan Fadhillah Arisandi <122199835+Shelford21@users.noreply.github.com>
2026-06-12 13:29:47 +0700

    Update NewLabelInfo.py

--- NewLabelInfo.py
+++ NewLabelInfo.py
@@ -38,6 +38,41 @@ group_files = {
 
 chat_file = group_files[selected_group]
 
+line_options = [
+    "A1",
+    "A2",
+    "A3",
+    "A4",
+    "A5",
+    "A6",
+    "A7",
+    "A8",
+    "A9",
+    "A10",
+
+    "B1",
+    "B2",
+    "B3",
+    "B4",
+    "B5",
+    "B6",
+    "B7",
+    "B8",
+    "B9",
+    "B10",
+
+    "C1",
+    "C2",
+    "C3",
+    "C4",
+    "C5",
+    "C6",
+    "C7",
+    "C8",
+    "C9",
+    "C10",
+]
+
 selected_line = st.selectbox(
     "Pilih LINE",
     line_options,
